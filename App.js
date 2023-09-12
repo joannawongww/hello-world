fe85fd0145da624ca01883ad24cf644537522185
joannawongww <joanna_1994@hotmail.com>
2023-09-12 10:45:41 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, Alert, ScrollView } from "react-native";
+import { useState } from "react";
 
-export default function App() {
+const App = () => {
+  const [text, setText] = useState("");
+
+  const alertMyText = () => {
+    Alert.alert(text)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={setText}
+        placeholder="type something here"
+      />
+      <Text style={styles.textDisplay}>You wrote: {text}</Text>
+      <Button
+        onPress={() => {
+          alertMyText();
+        }}
+        title="Press Me"
+      />
+      <ScrollView>
+        <Text style={{fontSize: 110}}>Text inside ScrollView!</Text>
+      </ScrollView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    alignItem: 'center',
+    justifyContent: 'center'
   },
+  textInput: {
+    width: '88%',
+    borderWidth: 1,
+    height: 50,
+    padding:10
+  },
+  textDisplay: {
+    height: 50,
+    lineHeight:50
+  }
+
 });
+
+export default App;
